b7609a7ffe31ca689dfba9db82c644e99ada940a
depcito <samuelh.echeverri@outlook.com>
2026-02-05 21:26:54 -0500

    new movie

--- movie/views.py
+++ movie/views.py
@@ -7,4 +7,5 @@ def home(request):
     return render(request, 'home.html')
     #return HttpResponse("Welcome to Home Page </h1>")
 def about(request):
-    return HttpResponse("Welcome to About Page </h1>")
+    #return HttpResponse("Welcome to About Page </h1>")
+    return render(request, 'about.html')
