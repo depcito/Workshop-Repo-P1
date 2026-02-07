3b159574cc8cebbe22d4c799b84a63a69d755975
depcito <samuelh.echeverri@outlook.com>
2026-02-06 19:05:19 -0500

    Actualiza proyecto

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Movie
 # Create your views here.
 
 def home(request):
+    searchterm = request.GET.get('searchMovie')
+    if searchterm:
+        movies = Movie.objects.filter(title__icontains=searchterm)
+    else:
+        movies=Movie.objects.all()
+    return render(request, 'home.html', {'searchterm': searchterm, 'movies': movies})
     return render(request, 'home.html', {'name': 'Samuel Hernando Echeverri Castrillon'})
+
     #return HttpResponse("Welcome to Home Page </h1>")
 def about(request):
     #return HttpResponse("Welcome to About Page </h1>")
